fcf220d48753ba0e177371c2822c7811e30170a3
Edutzu444 <34040754+Edutzu444@users.noreply.github.com>
2023-11-22 11:41:29 +0100

    update class

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,12 +1,16 @@
 package guru.springframework.spring5webapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Spring5webappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+	private String nome ;
+	private String cognome ;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 }
